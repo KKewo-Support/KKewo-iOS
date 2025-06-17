07056b4078770d0b3a303f072e8865723b97d9f3
YeoSeongil <seongil_yeo@naver.com>
2025-06-17 13:56:47 +0900

    [#16] feat: 네비 링크 연결

--- KKewo-iOS/KKewo-iOS/Feature/Onboarding/OnboardingView.swift
+++ KKewo-iOS/KKewo-iOS/Feature/Onboarding/OnboardingView.swift
@@ -8,72 +8,76 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    @State private var currentPage: Int = 0
-    
-    private let totalPage = 2
-    
-    var body: some View {
-        VStack {
-            HStack(spacing: 6) {
-                ForEach(0..<totalPage, id: \.self) { index in
-                    Circle()
-                        .frame(width: currentPage == index ? 12 : 10,
-                               height: currentPage == index ? 12 : 10)
-                        .foregroundStyle(currentPage == index ? .orange01 : .gray01)
-                        .animation(.easeInOut(duration: 0.3), value: currentPage)
-                }
-            }
-            
-            VStack(spacing: 0) {
-                Text("혼자 일어나기 힘든 러너들,")
-                    .font(.pretendard(type: .bold, size: 20))
-                    .foregroundStyle(.gray05)
-                
-                Text("멘토들이 깨워드릴게요!")
-                    .font(.pretendard(type: .bold, size: 28))
-                    .foregroundStyle(.gray05)
-            }
-            .padding(.top, 32)
-            
-            TabView(selection: $currentPage) {
-                OnboardingAlarmIntroView()
-                    .tag(0)
-                
-                OnboardingVoiceIntroView()
-                    .tag(1)
-            }
-            .padding(.top, 72)
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            
-            Spacer()
-            
-            Spacer()
-            
-            CustomBorderButton(action: currentPage == 0 ? handleNextAction : handleRecommendAction,
-                               title: currentPage == 0 ? "다음" : "알람벨 추천받기",
-                               titleColor: .white,
-                               backgroundColor: .orange01
-            )
-            .buttonStyle(.plain)
-            .padding(.horizontal, 24)
+  @State private var currentPage: Int = 0
+  @State private var isNext: Bool = false
+  
+  private let totalPage = 2
+  
+  var body: some View {
+    VStack {
+      HStack(spacing: 6) {
+        ForEach(0..<totalPage, id: \.self) { index in
+          Circle()
+            .frame(width: currentPage == index ? 12 : 10,
+                   height: currentPage == index ? 12 : 10)
+            .foregroundStyle(currentPage == index ? .orange01 : .gray01)
+            .animation(.easeInOut(duration: 0.3), value: currentPage)
         }
-        .padding(.vertical, 24)
+      }
+      
+      VStack(spacing: 0) {
+        Text("혼자 일어나기 힘든 러너들,")
+          .font(.pretendard(type: .bold, size: 20))
+          .foregroundStyle(.gray05)
+        
+        Text("멘토들이 깨워드릴게요!")
+          .font(.pretendard(type: .bold, size: 28))
+          .foregroundStyle(.gray05)
+      }
+      .padding(.top, 32)
+      
+      TabView(selection: $currentPage) {
+        OnboardingAlarmIntroView()
+          .tag(0)
+        
+        OnboardingVoiceIntroView()
+          .tag(1)
+      }
+      .padding(.top, 72)
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+      
+      Spacer()
+      
+      Spacer()
+      
+      CustomBorderButton(action: currentPage == 0 ? handleNextAction : handleRecommendAction,
+                         title: currentPage == 0 ? "다음" : "알람벨 추천받기",
+                         titleColor: .white,
+                         backgroundColor: .orange01
+      )
+      .buttonStyle(.plain)
+      .padding(.horizontal, 24)
     }
+    .padding(.vertical, 24)
+    .navigationDestination(isPresented: $isNext) {
+      MainView()
+    }
+  }
 }
 
 private extension OnboardingView {
-    func handleNextAction() {
-        withAnimation(.easeInOut) {
-            currentPage = 1
-        }
+  func handleNextAction() {
+    withAnimation(.easeInOut) {
+      currentPage = 1
     }
-    
-    func handleRecommendAction() {
-        
-    }
+  }
+  
+  func handleRecommendAction() {
+    isNext = true
+  }
 }
 
 #Preview {
-    OnboardingView()
+  OnboardingView()
 }
 
--- KKewo-iOS/KKewo-iOS/Feature/SelectSound/SelectSoundView.swift
+++ KKewo-iOS/KKewo-iOS/Feature/SelectSound/SelectSoundView.swift
@@ -8,138 +8,139 @@
 import SwiftUI
 
 struct SelectSoundView: View {
-    let soundCategories = ["전체", "신나는", "조용한", "시끄러운"]
-    let soundList = ["기본 벨소리", "Leeo의 감미로운 뮤지컬", "애원하는 Friday", "Ringo의 링고링고링~", "조심스러운 Howard"]
-    
-    @State private var selectedCategory: String = "전체"
-    @State private var selectedSound: String? = nil
-    
-    var body: some View {
+  let soundCategories = ["전체", "신나는", "조용한", "시끄러운"]
+  let soundList = ["기본 벨소리", "Leeo의 감미로운 뮤지컬", "애원하는 Friday", "Ringo의 링고링고링~", "조심스러운 Howard"]
+  
+  @State private var selectedCategory: String = "전체"
+  @State private var selectedSound: String? = nil
+  
+  var body: some View {
+    VStack{
+      HStack {
+        Button(action: {}) {
+          Image(systemName: "chevron.left")
+            .imageScale(.large)
+            .foregroundColor(.black)
+        }
+        Spacer()
+        
+        Text("벨소리")
+          .font(.custom("Pretendard-SemiBold", size: 18))
+          .frame(maxWidth: .infinity, alignment: .trailing)
+        Spacer()
+        
+        Button(action: {}) {
+          Text("완료")
+            .font(.custom("Pretendard-Regular", size: 20))
+            .foregroundColor(.orange)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+      }
+      .padding(.top, 10)
+      .padding(.bottom, 20)
+      .padding(.horizontal)
+      
+      // 알람 카테고리
+      HStack {
+        ForEach(soundCategories, id: \.self) { category in
+          Button(action:{
+            handleCategorySelection(category)
+          }) {
+            Spacer()
+            
+            Text(category)
+              .font(.custom("Pretendard-SemiBold", size: 16))
+              .foregroundColor(selectedCategory == category ? Color.white: Color.black)
+              .frame(maxWidth: .infinity, minHeight: 40)
+              .background(
+                RoundedRectangle(cornerRadius: 20)
+                  .fill(selectedCategory == category ? Color.black : Color.clear)
+              )
+              .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                  .stroke(Color.gray, lineWidth: 1)
+              )
+            Spacer()
+          }
+        }
+      }
+      
+      // 벨소리 선택
+      ScrollView {
         VStack{
-            HStack {
-                Button(action: {}) {
-                    Image(systemName: "chevron.left")
-                        .imageScale(.large)
-                        .foregroundColor(.black)
-                }
-                Spacer()
-                
-                Text("벨소리")
-                    .font(.custom("Pretendard-SemiBold", size: 18))
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                Spacer()
-                
-                Button(action: {}) {
-                    Text("완료")
-                        .font(.custom("Pretendard-Regular", size: 20))
-                        .foregroundColor(.orange)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                }
+          // 벨소리 추천받기 버튼
+          Button(action: {
+            print("추천하기")
+          }) {
+            HStack{
+              Image("soundRecommend")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 44, height: 44)
+                .padding(.trailing, 20)
+              
+              Text("벨소리 추천받기")
+                .font(.custom("Pretendard-SemiBold", size: 16))
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
-            .padding(.top, 10)
-            .padding(.bottom, 20)
-            .padding(.horizontal)
-            
-            // 알람 카테고리
-            HStack {
-                ForEach(soundCategories, id: \.self) { category in
-                    Button(action:{
-                        handleCategorySelection(category)
-                    }) {
-                        Spacer()
-                        
-                        Text(category)
-                            .font(.custom("Pretendard-SemiBold", size: 16))
-                            .foregroundColor(selectedCategory == category ? Color.white: Color.black)
-                            .frame(maxWidth: .infinity, minHeight: 40)
-                            .background(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(selectedCategory == category ? Color.black : Color.clear)
-                                )
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(Color.gray, lineWidth: 1)
-                                )
-                        Spacer()
-                    }
-                }
-            }
-            
-            // 벨소리 선택
-            ScrollView {
-                VStack{
-                    // 벨소리 추천받기 버튼
-                    Button(action: {
-                        print("추천하기")
-                    }) {
-                        HStack{
-                            Image("soundRecommend")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 44, height: 44)
-                                .padding(.trailing, 20)
-                            
-                            Text("벨소리 추천받기")
-                                .font(.custom("Pretendard-SemiBold", size: 16))
-                                .foregroundColor(.black)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 16)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                    }
-                    .padding(.horizontal)
-                    
-                    // 벨소리 목록
-                    ForEach(soundList, id: \.self) { sound in
-                        SoundList(
-                            sound: sound,
-                            isSelected: selectedSound == sound,
-                            onSelect: { selectedSound = sound },
-                            onPause: { print("\(sound) 일시정지") }
-                        )
-                    }
-                }
-            }
-            .padding(.vertical)
+            .padding()
+            .background(
+              RoundedRectangle(cornerRadius: 16)
+                .stroke(Color.gray, lineWidth: 1)
+            )
+          }
+          .padding(.horizontal)
+          
+          // 벨소리 목록
+          ForEach(soundList, id: \.self) { sound in
+            SoundList(
+              sound: sound,
+              isSelected: selectedSound == sound,
+              onSelect: { selectedSound = sound },
+              onPause: { print("\(sound) 일시정지") }
+            )
+          }
         }
+      }
+      .padding(.vertical)
+      .navigationBarBackButtonHidden()
     }
+  }
+  
+  // 카테고리 선택
+  private func handleCategorySelection(_ category: String) {
+    selectedCategory = category
     
-    // 카테고리 선택
-    private func handleCategorySelection(_ category: String) {
-        selectedCategory = category
-        
-        switch category {
-        case "전체":
-            handleAll()
-        case "신나는":
-            handleExciting()
-        case "조용한":
-            handleQuiet()
-        case "시끄러운":
-            handleNoisy()
-        default:
-            handleAll()
-        }
+    switch category {
+    case "전체":
+      handleAll()
+    case "신나는":
+      handleExciting()
+    case "조용한":
+      handleQuiet()
+    case "시끄러운":
+      handleNoisy()
+    default:
+      handleAll()
     }
+  }
+  
+  // 카테고리 함수 처리
+  private func handleAll() {
     
-    // 카테고리 함수 처리
-    private func handleAll() {
-        
-    }
-    private func handleExciting() {
-        
-    }
-    private func handleQuiet() {
-        
-    }
-    private func handleNoisy() {
-        
-    }
+  }
+  private func handleExciting() {
+    
+  }
+  private func handleQuiet() {
+    
+  }
+  private func handleNoisy() {
+    
+  }
 }
 
 #Preview {
-    SelectSoundView()
+  SelectSoundView()
 }
