86bf558ef646ebeddebe410e7c42e7cec2ff53fe
YeoSeongil <seongil_yeo@naver.com>
2025-06-17 13:56:28 +0900

    [#21] feat: 최상단 뷰 네비 스택 적용

--- KKewo-iOS/KKewo-iOS/KKewo_iOSApp.swift
+++ KKewo-iOS/KKewo-iOS/KKewo_iOSApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct KKewo_iOSApp: App {
-    var body: some Scene {
-        WindowGroup {
-            SelectSoundView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      NavigationStack {
+        OnboardingView()
+      }
     }
+  }
 }
