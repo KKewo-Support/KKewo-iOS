7eec6f6165b3fe3419ee186ad7666fc1b3d4b030
Seungjin Lee <sjin3687@gmail.com>
2025-06-23 22:51:45 +0900

    [#29] feat: 요일 정의 Weekday 추가

---
+++ KKewo-iOS/KKewo-iOS/Utilities/Enum/WeekDay.swift
@@ -0,0 +1,19 @@
+//
+//  WeekDay.swift
+//  KKewo-iOS
+//
+//  Created by 이승진 on 6/20/25.
+//
+
+import Foundation
+
+/// 요일 정의
+enum Weekday: String, Codable, CaseIterable {
+    case mon = "월"
+    case tue = "화"
+    case wed = "수"
+    case thu = "목"
+    case fri = "금"
+    case sat = "토"
+    case sun = "일"
+}
