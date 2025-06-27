3ead98048991c29f35e4ad57ec45de8aac0d9f1c
min-gyoung <alsrud0205@naver.com>
2025-06-27 14:05:19 +0900

    [Feat] 조건별 이미지 변경

--- KKewo-iOS/KKewo-iOS/Feature/SelectSound/SelectSoundView.swift
+++ KKewo-iOS/KKewo-iOS/Feature/SelectSound/SelectSoundView.swift
@@ -102,6 +102,7 @@ struct SelectSoundView: View {
             SoundList(
               sound: sound,
               isSelected: selectedSound == sound,
+              imageName: SoundName.imageName(for: sound),
               onSelect: {
                 if selectedSound == sound {
                   soundPlayer.pause()
--- KKewo-iOS/KKewo-iOS/Feature/SelectSound/SoundName.swift
+++ KKewo-iOS/KKewo-iOS/Feature/SelectSound/SoundName.swift
@@ -15,7 +15,18 @@ struct SoundName {
     case "애원하는 Friday": return "example"
     case "Ringo의 링고링고링~": return "example"
     case "조심스러운 Howard": return "example"
-    default: return "example" // default
+    default: return "example"
+    }
+  }
+  
+  static func imageName(for displayName: String) -> String {
+    switch displayName {
+    case "기본 벨소리": return "default"
+    case "Leeo의 감미로운 뮤지컬": return "leeo"
+    case "애원하는 Friday": return "friday"
+    case "Ringo의 링고링고링~": return "ringo"
+    case "조심스러운 Howard": return "howard"
+    default: return "soundRecommend"
     }
   }
 }
